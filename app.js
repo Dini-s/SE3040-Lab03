2ea80e1966920cadd30523c41946bc6b18497256
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2026-02-23 10:42:37 +0530

    add file reading example

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+import fs from 'fs';
+
 console.log("This is my First setup");
+
+//example 1
+fs.readFile('file.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log("file content:");
+    console.log(data);
+})
