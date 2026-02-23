c089660142f00631926ab51730c35605e57299e1
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2026-02-23 10:49:38 +0530

    add file eritting  example

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
     if (err) throw err;
     console.log("file content:");
     console.log(data);
-})
+});
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+    if (err) throw err;
+    console.log('File saved!');
+});
+
